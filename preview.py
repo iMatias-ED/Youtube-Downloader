d8fb08076c2996df6b146325413732b871fa4b3e
Matias-DJ <matDJ31@gmail.com>
2021-06-07 21:16:41 -0400

    functionality for download the video

--- preview.py
+++ preview.py
@@ -1,3 +1,4 @@
+from sys import dont_write_bytecode
 from PySide2.QtWidgets import *
 from PySide2.QtGui import QImage, QPixmap
 from PySide2.QtCore import *
@@ -67,20 +68,21 @@ class PreviewFrame(Frame):
                 }
             '''
         )
+        self.downloadButton.clicked.connect(self.getDirectoryPath)
     
     def videoData(self, url):
         print("funcion video data llamada")
-        video = YoutubeVideo(url)
+        self.video = YoutubeVideo(url)
 
-        self.title.setText(video.title)
+        self.title.setText(self.video.title)
 
         #charge video thumbnail
-        url_image = video.thumbnail_url
+        url_image = self.video.thumbnail_url
         manager = QNetworkAccessManager(self)
         manager.finished.connect(self.onFinished)
         manager.get(QNetworkRequest(QUrl(url_image)))
 
-        resolutions = video.streams.filter(file_extension="mp4")
+        resolutions = self.video.streams.filter(file_extension="mp4", progressive=True)
         items = []
 
         for i in resolutions:
@@ -105,6 +107,21 @@ class PreviewFrame(Frame):
 
         self.thumbnail.setPixmap(image)
         
+    def getDirectoryPath(self):
+        path = QFileDialog.getExistingDirectory(self, "Seleccione el directorio de descarga")
+        quality = self.quality.currentText()
+        self.downloadVideo(quality, path)
 
+
+    def downloadVideo(self, quality, path):
+        select_video = self.video.streams.filter(resolution=quality, progressive=True)
+        
+        start = str(select_video).find("\"")
+        end = str(select_video).find("mime_type")
+        itag = str(select_video)[start+1 :end-2]
+        itag = int(itag)
+
+        stream = self.video.streams.get_by_itag(itag)
+        stream.download(path)
         
     
